69400ff7602bd4c4ea30af6824a66c50993b78fa
Apursuit <apursuit27@gmail.com>
2024-05-23 17:55:12 +0800

    update main branch

--- deploy-main.sh
+++ deploy-main.sh
@@ -4,22 +4,20 @@
 set -e
 
 # 设置变量
-push_addr=git@github.com:Apursuit/Apursuit.github.io.git # git 提交地址
-commit_info='commit'
 push_branch=main # 推送的分支
 
 echo "开始部署脚本..."
-echo "Git 提交地址: $push_addr"
-echo "提交信息: $commit_info"
-echo "推送的分支: $push_branch"
 
 # 添加所有文件并提交
 echo "添加所有文件并提交"
 git add .
 git commit -m "update main branch"
 
+# 临时设置提交地址
+push_addr="git@github.com:Apursuit/Apursuit.github.io.git" # 临时 git 提交地址
+
 # 强制推送到远程仓库的指定分支
 echo "推送到远程仓库"
-git push -f origin $push_branch
+git push -f $push_addr $push_branch
 
 echo "部署完成"
