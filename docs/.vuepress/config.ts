2036999a9c1ce42fbc46bbd00b9a76f69c7da65d
Apursuit <apursuit27@gmail.com>
2024-06-26 11:27:59 +0800

    commit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -279,9 +279,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       '@vuepress/last-updated', // "上次更新"时间格式
       {
         transformer: (timestamp, lang) => {
-          const moment = require('moment')
-          return moment(timestamp).format('YYYY/MM/DD, HH:mm:ss')
-          // return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
         },
       },
     ],
